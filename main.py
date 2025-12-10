4846f77bf545d95a25545f455c07e4cf536b1fba
Gabriel Salustiano <gabriel.salustiano.peres@gmail.com>
2025-12-10 15:10:08 -0300

    jumpscare first prototype

---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+import random
+import time 
+import os
+
+CHANCE = 1 #chance por segundo (1 em X)
+FRAME_RATE = 30 #framerate do jumpscare
+
+def load_frames(folder):
+    frames = []
+    for file in sorted(os.listdir(folder)):
+        if file.endswith(".png"):
+            path = os.path.join(folder, file)
+            img = pygame.image.load(path)
+            frames.append(img)
+    return frames
+
+def show_jumpscare(screen, frames, sound):
+    sound.play()
+    
+    clock = pygame.time.Clock()
+
+    for frame in frames:
+        frame = pygame.transform.scale(frame, (screen.get_width(), screen.get_height()))
+        screen.blit(frame,(0, 0))
+        pygame.display.flip()
+        clock.tick(FRAME_RATE)
+    
+    time.sleep(0.3)
+
+def main():
+    pygame.init()
+    pygame.mixer.init()
+
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("")
+
+    frames = load_frames("frames")
+    sound = pygame.mixer.Sound("jumpscare.mp3")
+
+    while True:
+        time.sleep(1)
+
+        if random.randint(1, CHANCE) == 1:
+            show_jumpscare(screen, frames, sound)
+
+            for event in pygame.event.get():
+                pass
+
+if __name__ == "__main__":
+    main()
